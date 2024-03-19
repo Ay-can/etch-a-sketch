a041ea9e7b4cf616e3c33b85478605065a53f31b
ay-can <aycan.acn2020@gmail.com>
2024-03-19 23:44:45 +0100

    use prettier to format with js standard of 2 spaces

--- script.js
+++ script.js
@@ -2,70 +2,76 @@ const container = document.querySelector(".container");
 const btn = document.querySelector("#btn-grid");
 
 function createGrid(gridSize) {
-    for (let i = 0; i < gridSize * gridSize; i++) {
-        const div = document.createElement("div");
-        //div.style.border = "1px solid black";
-        div.style.width = 400 / gridSize + "px";
-        div.style.height = 400 / gridSize + "px";
-        div.style.flex = "auto";
-        container.appendChild(div);
-    }
+  for (let i = 0; i < gridSize * gridSize; i++) {
+    const div = document.createElement("div");
+    //div.style.border = "1px solid black";
+    div.style.width = 400 / gridSize + "px";
+    div.style.height = 400 / gridSize + "px";
+    div.style.flex = "auto";
+    container.appendChild(div);
+  }
 }
-createGrid(16);
-drawBox();
 
 function deleteGrid() {
-    const boxes = document.querySelectorAll(".container > div");
-    boxes.forEach((box) => {
-        container.removeChild(box);
-    })
+  const boxes = document.querySelectorAll(".container > div");
+  boxes.forEach((box) => {
+    container.removeChild(box);
+  });
 }
 
 btn.addEventListener("click", (event) => {
-    let gridSize = prompt("Enter a number between 10 and 100");
-    if (gridSize < 10 || gridSize > 100) {
-        alert("Enter a number between 10 and 100!");
-        return;
-    }
+  let gridSize = prompt("Enter a number between 10 and 100");
+  if (gridSize < 10 || gridSize > 100) {
+    alert("Enter a number between 10 and 100!");
+    return;
+  }
 
-    deleteGrid();    
-    createGrid(gridSize);
-    drawBox();
-})
+  deleteGrid();
+  createGrid(gridSize);
+  drawBox();
+});
 
 let isErasing = false;
 const eraserBtn = document.querySelector("#btn-eraser");
 eraserBtn.addEventListener("click", () => {
-    isErasing = true;
+  isErasing = true;
 });
 
 let isRainbow = true;
 const rainbowBtn = document.querySelector("#btn-rainbow");
 rainbowBtn.addEventListener("click", () => {
-   isRainbow = true; 
-   isErasing = false;
+  isRainbow = true;
+  isErasing = false;
 });
 
 function drawBox() {
-    const boxes = document.querySelectorAll(".container > div");
-    boxes.forEach((box) => {
-        box.addEventListener("mouseenter", (event) => {
-            if (!isErasing && isRainbow) {
-                event.target.style.backgroundColor = getRandomColor();
-                event.target.style.opacity = Number(event.target.style.opacity) + 0.1;
-            } else {
-                event.target.style.backgroundColor = "white";
-                event.target.style.opacity = 0;
-            }
-        });
-    })
+  const boxes = document.querySelectorAll(".container > div");
+  boxes.forEach((box) => {
+    box.addEventListener("mouseenter", (event) => {
+      if (!isErasing && isRainbow && !isColorPicking) {
+        event.target.style.backgroundColor = getRandomColor();
+        event.target.style.opacity = Number(event.target.style.opacity) + 0.1;
+      } else {
+        event.target.style.backgroundColor = "white";
+        event.target.style.opacity = 0;
+      }
+    });
+  });
 }
 
 function getRandomColor() {
-    const red = Math.floor(Math.random() * 256);
-    const green = Math.floor(Math.random() * 256);
-    const blue = Math.floor(Math.random() * 256);
-    const rgb = `rgb(${red},${green}, ${blue})`;    
+  const red = Math.floor(Math.random() * 256);
+  const green = Math.floor(Math.random() * 256);
+  const blue = Math.floor(Math.random() * 256);
+  const rgb = `rgb(${red},${green}, ${blue})`;
 
-    return rgb;
+  return rgb;
 }
+
+function init() {
+  createGrid(16);
+  drawBox();
+}
+
+init();
+// Add color picker
